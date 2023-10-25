fbc361306002350cdfd31b711c8cfcd4b0acbe5b
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-10-26 01:08:19 +0700

    Update PageBuilder.php

--- plugins/BuilderPage/src/Livewire/PageBuilder.php
+++ plugins/BuilderPage/src/Livewire/PageBuilder.php
@@ -22,6 +22,7 @@ class PageBuilder extends Component
             Item::Add('name')->Column(Item::Col12)->Title('Title')->Required(),
             Item::Add('slug')->Column(Item::Col12)->Title('Subdomain')->Type('subdomain'),
             Item::Add('description')->Column(Item::Col12)->Type('textarea')->Title('Description'),
+            Item::Add('image')->Column(Item::Col12)->Type('images')->Title('Featured image'),
             Item::Add('css')->Title('Css')->Type('textarea')->Column(Item::Col12),
             Item::Add('js')->Title('Js')->Type('textarea')->Column(Item::Col12),
             Item::Add('published_at')->Column(Item::Col12)->Type('flatpickr')->ValueDefault(function () {
