79451de806ed892516f6fc0c8164706e399cb0a4
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-11-04 02:20:01 +0700

    Update function.php

--- helpers/function.php
+++ helpers/function.php
@@ -2,6 +2,11 @@
 
 use BytePlatform\Facades\Assets;
 use BytePlatform\Facades\Shortcode;
+use BytePlatform\Seo\Facades\SEO;
+use BytePlatform\Seo\SchemaCollection;
+use BytePlatform\Seo\Schemas\ArticleSchema;
+use BytePlatform\Seo\Schemas\BreadcrumbListSchema;
+use BytePlatform\Seo\SEOData;
 
 if (!function_exists('pagebuilder_render')) {
     function pagebuilder_render($data)
@@ -14,7 +19,13 @@ if (!function_exists('pagebuilder_render')) {
         Assets::AddStyle(trim($data->css));
         Assets::AddScript($data->custom_js);
         Assets::AddStyle(trim($data->custom_css));
+        SEO::SEODataTransformer(function (SEOData $data) {
+            $data->schema = SchemaCollection::initialize()->addArticle(function (ArticleSchema $articleSchema) {
+            })->addBreadcrumbs(function (BreadcrumbListSchema $breadcrumbListSchema) {
+                $breadcrumbListSchema->appendBreadcrumbs([]);
+            });
+            return $data;
+        });
         return $data->content;
     }
 }
-
