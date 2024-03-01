ab31addbd640700a23a17458772fe4b5a97e90f8
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-03-01 15:53:13 +0700

    Update BuilderServiceProvider.php

--- src/BuilderServiceProvider.php
+++ src/BuilderServiceProvider.php
@@ -61,44 +61,14 @@ class BuilderServiceProvider extends ServiceProvider
     {
         $this->bootGate();
         Platform::Ready(function () {
-            if (sokeio_is_admin()) {
-                add_filter('CMS_PAGE_BUTTONS', function ($prev) {
-                    return [
-                        UI::Button(__('Create With Builder'))->Route('admin.builder.page.new'),
-                        ...$prev,
-                    ];
-                });
-                add_filter('CMS_PAGE_TABLE_ACTIONS', function ($prev) {
-                    return [
-                        UI::Button(__('Edit With Builder'))->Cyan()->Route('admin.builder.page.edit', function ($row) {
-                            return ['dataId' => $row->id];
-                        }),
-                        ...$prev,
-                    ];
-                });
-            }
-            add_filter('CMS_POST_BUTTONS', function ($prev) {
-                return [
-                    UI::Button(__('Create With Builder'))->Route('admin.builder.post.new'),
-                    ...$prev,
-                ];
+            Menu::Register(function () {
+                if (sokeio_is_admin()) {
+                    menu_admin()->subMenu(__('Builder Manager'), '', function (MenuBuilder $menu) {
+                        $menu->route(['name' => 'admin.builder-template', 'params' => []], __('Template'), '', [], 'admin.builder-template');
+                        $menu->route(['name' => 'admin.builder-plugin', 'params' => []], __('Plugin'), '', [], 'admin.builder-plugin');
+                    }, 1000);
+                }
             });
-            add_filter('CMS_POST_TABLE_ACTIONS', function ($prev) {
-                return [
-                    UI::Button(__('Edit With Builder'))->Cyan()->Route('admin.builder.post.edit', function ($row) {
-                        return ['dataId' => $row->id];
-                    }),
-                    ...$prev,
-                ];
-            });
-        });
-        Menu::Register(function () {
-            if (sokeio_is_admin()) {
-                menu_admin()->subMenu(__('Builder Manager'), '', function (MenuBuilder $menu) {
-                    $menu->route(['name' => 'admin.builder-template', 'params' => []], __('Template'), '', [], 'admin.builder-template');
-                    $menu->route(['name' => 'admin.builder-plugin', 'params' => []], __('Plugin'), '', [], 'admin.builder-plugin');
-                }, 1000);
-            }
         });
     }
 }
